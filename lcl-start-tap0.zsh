a88dd4440cd1f8bd41a82dc3860955360a4164dc
Andy Bruce <andybrucenet@gmail.com>
2017-07-28 09:46:36 -0400

    Updates to fix the DNS loss

--- lcl-start-tap0.zsh
+++ lcl-start-tap0.zsh
@@ -3,10 +3,11 @@
 [[ "$UID" -ne "0" ]] && echo "You must be root. Goodbye..." && exit 1
 echo "starting"
 exec 4<>/dev/tap0
-ifconfig tap0 10.10.10.1 10.10.10.255
-#ifconfig tap0 192.168.98.100/24
+#ifconfig tap0 10.10.10.1 10.10.10.255
+ifconfig tap0 192.168.98.100 192.168.98.255
 ifconfig tap0 up
-ping -c1 10.10.10.1
+#ping -c1 10.10.10.1
+ping -c1 -t 3 192.168.98.100
 echo "ending"
 export PS1="tap interface>"
 dd of=/dev/null <&4 & # continuously reads from buffer and dumps to null
