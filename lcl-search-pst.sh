76dfaebd7ce69a23d65c45012f051f379bb5f597
Andy Bruce <andybrucenet@gmail.com>
2017-08-14 11:31:35 -0400

    Now allows replaceable file parms/help text

--- lcl-search-pst.sh
+++ lcl-search-pst.sh
@@ -15,9 +15,13 @@ do_search_1() {
   local l_tmp="/tmp/$$.foo"
   local l_i=''
   local l_j=''
+  local l_process=''
 
   # remove existing
   rm -f *.pst
+
+  # construct tmp file for command
+  l_process="/tmp/lcl-search-pst.$$"
 
   # decompress to local folder
   7z e -y "$i_file" > "$l_tmp" 2>&1
@@ -37,8 +41,28 @@ do_search_1() {
     # iterate over all PST output folder contents
     for l_j in ./pst-output/* ; do
       #set -x
-      "$@" "$l_j"
-      set +x
+      echo '#!/bin/bash' > "$l_process"
+      for i in "$@" ; do
+        if echo "$i" | grep --quiet -e ' ' ; then
+          echo -n "'$i' " >> "$l_process"
+        else
+          echo -n "$i " >> "$l_process"
+        fi
+      done
+      if grep --quiet -e '{}' "$l_process" ; then
+        # replace with filename
+        sed -i -e "s#{}#'$l_j'#g" "$l_process"
+        echo '' >> "$l_process"
+      else
+        echo " '$l_j'" >> "$l_process"
+      fi
+      echo '' >> "$l_process"
+      chmod +x "$l_process"
+      #cat "$l_process"
+      "$l_process"
+      l_rc=$?
+      rm -f "$l_process"
+      [ $l_rc -ne 0 ] && break
     done
   done
 
@@ -46,6 +70,17 @@ do_search_1() {
   rm -f "$l_tmp"
   return $l_rc
 }
+
+# handle empty
+if [ x"$1" = x ] ; then
+  echo "Usage: $(basename $0) [-x [PST-FILE] [-x [PST-FILE]] ...] [...CMD...]"
+  echo '  CMD - usually grepmail; insert filename at {} (at end if not given)'
+  echo 'Example (file appended to end):'
+  echo "  $(basename $0) -x 20170101-backup.pst 'grepmail -H -i -e \"MyProject01\"'"
+  echo 'Example (with file insertion):'
+  echo "  $(basename $0) -x 20170101-backup.pst 'grepmail -H -i -e \"MyProject01\" {} && do-some-action'"
+  exit 1
+fi
 
 # make PST output folder
 mkdir -p ./pst-output
