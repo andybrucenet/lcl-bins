568e2c71f1ebfaa92f7fd5b8ec488eb801be7207
Andy Bruce <andybrucenet@gmail.com>
2020-03-25 16:27:31 -0400

    Updated to have better prompts

--- lcl-disable-brew-quarantine-bit.sh
+++ lcl-disable-brew-quarantine-bit.sh
@@ -9,28 +9,41 @@ the_sed=$(which gsed 2>/dev/null)
 [ x"$the_sed" = x ] && the_sed=$(which sed)
 #echo "Using '$the_sed'"
 
-# locate the chef-user
-the_chef_user=$(ls -ld $(which brew) | awk '{print $3}')
+# locate the brew-user
+the_brew_user=$(ls -ld $(which brew) | awk '{print $3}')
 
 # these are in here to allow this script to work directly in chef recipe
 fool_ruby_parser='\'
 fool_ruby_parser+='1'
 
-# are we the current chef user?
-if [ x"$the_chef_user" != x"$THE_CUR_USER" ] ; then
+# are we the current brew user?
+if [ x"$the_brew_user" != x"$THE_CUR_USER" ] ; then
   # run command as different user
-  echo "Enter password for '$the_chef_user' when prompted..."
+  echo "We are user '$THE_CUR_USER'."
+  echo "We must become brew user '$the_brew_user'."
+  echo "**Enter password for '$the_brew_user' when prompted:"
   the_rc=1
   while [ $the_rc -ne 0 ] ; do
-    su $the_chef_user -c "$THE_PATH"
+    su $the_brew_user -c "$THE_PATH"
     the_rc=$?
     [ $the_rc -ne 0 ] && echo 'Try again...'
   done
-	exit $the_rc
+  exit $the_rc
 fi
 
+# now we are the current brew user - issue a sudo cmd
+echo ''
+echo "We are now the brew user '$the_brew_user'."
+echo "We are executing a privileged command."
+echo "**Enter '$the_brew_user' password if prompted:"
+sudo echo 'Privileged command executed OK.'
+the_rc=$?
+[ $the_rc -ne 0 ] && echo 'Failed privileged command. Exiting.' && exit $the_rc
+
 # iterate over all installed casks
-for i in $(sudo -u $the_chef_user brew cask list) ; do
+echo ''
+echo '**Unquarantine brew applications...'
+for i in $(sudo -u $the_brew_user brew cask list) ; do
   echo -n "Unquarantine $i: "
   the_app_path=''
   the_dir=$(find /usr/local/Caskroom -name $i -type d 2>/dev/null)
@@ -38,16 +51,16 @@ for i in $(sudo -u $the_chef_user brew cask list) ; do
   [ x"$the_dir" = x ] && echo 'empty the_dir' && continue
 
   # are there artifacts available?
-  if sudo -u $the_chef_user brew cask info $i 2>/dev/null | grep -ie '^==> Artifacts' >/dev/null 2>&1 ; then
+  if sudo -u $the_brew_user brew cask info $i 2>/dev/null | grep -ie '^==> Artifacts' >/dev/null 2>&1 ; then
     # *must* have an app
-    the_app_name=$(sudo -u $the_chef_user brew cask info $i | grep -A10 -ie '^==> Artifacts' | grep -ie '(App)' | $the_sed -e "s/^\(.*\) \+(App)/$fool_ruby_parser/i")
+    the_app_name=$(sudo -u $the_brew_user brew cask info $i | grep -A10 -ie '^==> Artifacts' | grep -ie '(App)' | $the_sed -e "s/^\(.*\) \+(App)/$fool_ruby_parser/i")
     [ x"$the_app_name" != x ] && the_app_path="/Applications/$the_app_name"
   fi
 
   # if no app lookup from install file
   if [ x"$the_app_path" = x ] ; then
     # get the install file first
-    the_installer_file=$(sudo -u $the_chef_user brew cask info $i | grep -ie '^from: ' | $the_sed -e "s/^from: \+\(.*\)/$fool_ruby_parser/i; s/^.*\/\([^\/]\+\)\$/$fool_ruby_parser/" | head -n 1)
+    the_installer_file=$(sudo -u $the_brew_user brew cask info $i | grep -ie '^from: ' | $the_sed -e "s/^from: \+\(.*\)/$fool_ruby_parser/i; s/^.*\/\([^\/]\+\)\$/$fool_ruby_parser/" | head -n 1)
     l_rc=$? ; [ $l_rc -ne 0 ] && echo "brew cask info fail with $l_rc" && continue
     [ x"$the_installer_file" = x ] && echo 'empty the_installer_file' && continue
 
