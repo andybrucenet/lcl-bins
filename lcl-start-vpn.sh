f49d2c965715484897ef69ff324d19e28d8ee26b
Andrew Bruce <abruce@lbambprox200.ada.hlsdev.com>
2020-04-02 15:19:20 -0400

    Made into small program

--- lcl-start-vpn.sh
+++ lcl-start-vpn.sh
@@ -4,8 +4,14 @@
 # vpn name
 the_vpn_name='vpn-lmil'
 
+# ping location
+PING='/sbin/ping'
+
+##############################################################
+# *internal functions*
+#
 # connect to the VPN
-function vpn-connect {
+function lcl-start-vpn-i-connect {
   /usr/bin/env osascript <<-EOF
 tell application "System Events"
   tell current location of network preferences
@@ -20,8 +26,9 @@ tell application "System Events"
 end tell
 EOF
 }
-
-function vpn-disconnect {
+#
+# disconnect
+function lcl-start-vpn-i-disconnect {
   /usr/bin/env osascript <<-EOF
 tell application "System Events"
   tell current location of network preferences
@@ -33,21 +40,64 @@ return
 EOF
 }
 
+##############################################################
+# external functions
+#
+# is vpn connected?
+function lcl-start-vpn-x-is-running {
+  # only run if we are online
+  if ! $PING -c 1 google.com >/dev/null 2>&1 ; then
+    echo 'Not online...ignoring'
+    return 1
+  fi
 
+  # do we already have VPN address?
+  if ip a show ppp0 >/dev/null 2>&1 ; then
+    return 0
+  fi
 
-# only run if we are online
-if ! ping -c 1 google.com >/dev/null 2>&1 ; then
-  echo 'Not online...ignoring'
-  exit 1
+  return 2
+}
+# connect to vpn
+function lcl-start-vpn-x-connect {
+  # only run if we are online
+  if ! $PING -c 1 google.com >/dev/null 2>&1 ; then
+    echo 'Not online...ignoring'
+    return 1
+  fi
+
+  # do we already have VPN address?
+  if ip a show ppp0 >/dev/null 2>&1 ; then
+    echo 'VPN already up'
+    return 2
+  fi
+
+  # start it up
+  echo 'Starting VPN...'
+  lcl-start-vpn-i-connect
+}
+#
+# disconnect from vpn
+function lcl-start-vpn-x-disconnect {
+  # do we already have VPN address?
+  if ! ip a show ppp0 >/dev/null 2>&1 ; then
+    echo 'VPN already down'
+    return 2
+  fi
+
+  # shut it down
+  echo 'Stopping VPN...'
+  lcl-start-vpn-i-disconnect
+}
+
+########################################################################
+# optional call support
+l_do_run=0
+if [ "x$1" != "x" ]; then
+  [ "x$1" != "xsource-only" ] && l_do_run=1
 fi
-
-# do we already have VPN address?
-if ip a show ppp0 >/dev/null 2>&1 ; then
-  echo 'VPN is up'
-  exit 0
+if [ $l_do_run -eq 1 ]; then
+  l_func="$1"; shift
+  [ x"$l_func" != x ] && eval lcl-start-vpn-x-"$l_func" "$@"
 fi
-
-# start it up
-cho 'Starting VPN...'
-vpn-connect
 
