e99f87d0790f876848292dbfe4c7261305f6fd04
Andy Bruce <andybrucenet@gmail.com>
2021-08-31 12:05:20 -0400

    Updates from new mac

--- lcl-disable-brew-quarantine-bit.sh
+++ lcl-disable-brew-quarantine-bit.sh
@@ -7,7 +7,7 @@ THE_CUR_USER="$(whoami)"
 # locate sed (latest brew installs only to gsed)
 the_sed=$(which gsed 2>/dev/null)
 [ x"$the_sed" = x ] && the_sed=$(which sed)
-#echo "Using '$the_sed'"
+echo "Using '$the_sed'"
 
 # locate the brew-user
 the_brew_user=$(ls -ld $(which brew) | awk '{print $3}')
@@ -45,6 +45,7 @@ echo ''
 echo '**Unquarantine brew applications...'
 for i in $(sudo -u $the_brew_user brew list --casks) ; do
   echo -n "Unquarantine $i: "
+  the_app_paths=''
   the_app_path=''
   the_dir=$(find /usr/local/Caskroom -name $i -type d 2>/dev/null)
   l_rc=$? ; [ $l_rc -ne 0 ] && echo "find /usr/local/Caskroom fail with $l_rc" && continue
@@ -53,13 +54,13 @@ for i in $(sudo -u $the_brew_user brew list --casks) ; do
   # are there artifacts available?
   if sudo -u $the_brew_user brew info --cask $i 2>/dev/null | grep -ie '^==> Artifacts' >/dev/null 2>&1 ; then
     # *must* have an app
-    the_app_name=$(sudo -u $the_brew_user brew info --cask $i | grep -A10 -ie '^==> Artifacts' | grep -ie '(App)' | $the_sed -e "s/^\(.*\) \+(App)/$fool_ruby_parser/i")
+    the_app_name=$(sudo -u $the_brew_user brew info --cask $i | grep -A10 -ie '^==> Artifacts' | grep -ie '(App)' | head -n 1 | $the_sed -e "s/^\(.*\)  *(App)/$fool_ruby_parser/i")
     if echo "$the_app_name" | grep -e ' -> ' >/dev/null 2>&1 ; then
       # link specified - extract
       the_app_name=$(echo "$the_app_name" | awk -F' -> ' '{print $2}')
       if echo "$the_app_name" | grep -e ')$' ; then
         # some kind of parenthetical indicator
-        the_app_name=$(echo "$the_app_name" | sed -e 's/^\(.*\) ([A-Za-z]\+)$/\1/')
+        the_app_name=$(echo "$the_app_name" | sed -e 's/^\(.*\) ([A-Za-z][A-Za-z]*)$/\1/')
       fi
     fi
     [ x"$the_app_name" != x ] && echo "$the_app_path" | grep -v '/' >/dev/null 2>&1 && the_app_path="/Applications/$the_app_name"
@@ -68,7 +69,7 @@ for i in $(sudo -u $the_brew_user brew list --casks) ; do
   # if no app lookup from install file
   if [ x"$the_app_path" = x ] ; then
     # get the install file first
-    the_installer_file=$(sudo -u $the_brew_user brew info --cask $i | grep -ie '^from: ' | $the_sed -e "s/^from: \+\(.*\)/$fool_ruby_parser/i; s/^.*\/\([^\/]\+\)\$/$fool_ruby_parser/" | head -n 1)
+    the_installer_file=$(sudo -u $the_brew_user brew info --cask $i | grep -ie '^from: ' | $the_sed -e "s/^from:  *\(.*\)/$fool_ruby_parser/i; s/^.*\/\([^\/][^\/]*\)\$/$fool_ruby_parser/" | head -n 1)
     l_rc=$? ; [ $l_rc -ne 0 ] && echo "brew info --cask fail with $l_rc" && continue
     [ x"$the_installer_file" = x ] && echo 'empty the_installer_file' && continue
 
@@ -87,8 +88,9 @@ for i in $(sudo -u $the_brew_user brew list --casks) ; do
   [ ! -d "$the_app_path" ] && echo "missing app '$the_app_path'" && continue
 
   # disable the quarantine bit on the discovered application
-  sudo xattr -r -d com.apple.quarantine "$the_app_path"
+  sudo xattr -s -r -d com.apple.quarantine "$the_app_path"
   l_rc=$? ; [ $l_rc -ne 0 ] && echo "xattr fail $l_rc" && continue
   echo 'OK'
 done
+set +x
 
